acdafeb0dbe704f3bc24d9e4144dd653604a4ad2
Engr. Rasel <mircityhousingltd@gmail.com>
2026-09-13 17:00:55 +0600

    Add Electron desktop launcher

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 950,
+    minWidth: 1100,
+    minHeight: 700,
+    backgroundColor: '#eef3f8',
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false
+    }
+  });
+  win.loadFile(path.join(__dirname, 'app', 'index.html'));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
